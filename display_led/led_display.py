3a895815790167e572382e114c3ecbd81ff3cfc3
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-05 15:58:00 -0300

    error handling version

--- display_led/led_display.py
+++ display_led/led_display.py
@@ -1,20 +1,28 @@
-num = input('ingrese un numero entero no negativo: ')
-list = []
+def ledDisplay(prompt):
+    while True:
+        try:
+            num = input(prompt)
+            if num >= 0:
+                list = []
+                for a in num:
+                    list.append(int(a))
+                    ls = [
+                            ['###','  #','###','###','# #','###','###','###','###','###'],
+                            ['# #','  #','  #','  #','# #','#  ','#  ','  #','# #','# #'],
+                            ['# #','  #','###','###','###','###','###','  #','###','###'],
+                            ['# #','  #','#  ','  #','  #','  #','# #','  #','# #','  #'],
+                            ['###','  #','###','###','  #','###','###','  #','###','###']
+                            ]
+                    for r in range(5):
+                        e = ' '
+                        for i in range(len(list)):
+                            if i == len(list)-1:
+                                e = '\n'
+                                print(ls[r][list[i]], end=e)
+            else:
+                ledDisplay(prompt)
+        except ValueError:
+            print('The input must be a non negative integer.\nPlease try again!')
+            ledDisplay(prompt)
 
-for a in num:
-    list.append(int(a))
-
-ls = [
-        ['###','  #','###','###','# #','###','###','###','###','###'],
-        ['# #','  #','  #','  #','# #','#  ','#  ','  #','# #','# #'],
-        ['# #','  #','###','###','###','###','###','  #','###','###'],
-        ['# #','  #','#  ','  #','  #','  #','# #','  #','# #','  #'],
-        ['###','  #','###','###','  #','###','###','  #','###','###']
-     ]
-
-for r in range(5):
-    e = ' '
-    for i in range(len(list)):
-        if i == len(list)-1:
-            e = '\n'
-        print(ls[r][list[i]], end=e)
+    ledDisplay('Enter a non negative number')
