5c94cdc0a9f0ec5d46ad7295b09f6ba9d4a63ccc
Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-08 21:42:03 -0300

    fixed the incorrect output for empty lines

--- Blueprints/numbersProcessor.py
+++ Blueprints/numbersProcessor.py
@@ -1,11 +1,17 @@
 #Procesador de números
+def numProcessor(prompt = 'Ingresa una línea de números, sepáralos con espacios: '):
+    while True:
+        linea = input(prompt)
+        if linea is not '':
+            break
 
-linea = input("Ingresa una línea de números, sepáralos con espacios: ")
-strings = linea.split()
-total = 0
-try:
-    for substr in strings:
-        total += float(substr)
-    print("El total es:", total)
-except:
-    print(substr, "no es un numero.")
+    strings = linea.split()
+    total = 0
+    try:
+        for substr in strings:
+            total += float(substr)
+        print("El total es:", total)
+    except:
+        print(substr, "no es un numero.")
+
+numProcessor()
