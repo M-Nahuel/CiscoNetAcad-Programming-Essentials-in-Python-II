6a67b56e6d57dd2fe716ebc7278d91b83deff758
Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-28 20:36:37 -0400

    Update sudoku.py
    line 22 raises exception TypeError: 'int' object is not subscriptable

--- unfinished/sudoku/sudoku.py
+++ unfinished/sudoku/sudoku.py
@@ -1,5 +1,5 @@
 def sudoku():
-    list = []
+    list = [295743861, 431865927, 876192543, 387459216, 612387495, 549216738, 763524189, 928671354, 154938672]
     
     #Tester for the columns
     def ctester():
@@ -14,11 +14,12 @@ def sudoku():
         return True
     
     
-    for i in range(9):
-        list.append([input('Ingrese 9 numeros para rellenar la fila: ')])
+    #for i in range(9):
+    #    list.append([input('Ingrese 9 numeros para rellenar la fila: ')])
     
     for j in range(9):
         for k in range(9):
+            print(list[j][k])
             if int(list[j][k]) > 9:
                 return 'Los numeros ingresados no deben ser mayor a 9!'
             if list[j].find(k+1) == -1:
