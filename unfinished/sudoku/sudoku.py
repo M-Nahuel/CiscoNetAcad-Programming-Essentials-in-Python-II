823bf53ca9fc1f420bf06ab54f83569e9a9742ed
Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-03-29 12:54:28 -0400

    Update sudoku.py
    toDo: fix the 3x3 boxes verification

--- unfinished/sudoku/sudoku.py
+++ unfinished/sudoku/sudoku.py
@@ -1,5 +1,5 @@
 def sudoku():
-    list = [295743861, 431865927, 876192543, 387459216, 612387495, 549216738, 763524189, 928671354, 154938672]
+    list = ['195743862', '431865927', '876192543', '387459216', '612387495', '549216738', '763524189', '928671354', '254938671']
     
     #Tester for the columns
     def ctester():
@@ -7,25 +7,36 @@ def sudoku():
             test = ''
             for k in range(9):
                 test += list[k][j]
-                if k == 8:
+                if int(k) == 8:
                     for l in range(9):
-                        if test.find(l) == -1:
+                        if test.find(str(l+1)) == -1:
                             return False
         return True
-    
+    #Tester for the 3 x 3 box
+    def btester():
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                checker = []
+                for k in range(3):
+                    for l in range(3):
+                        checker.append(list[i+k][j+l])
+        for a in range(9):
+            if str(a) not in checker:
+                return 'No'
+    btester()
     
     #for i in range(9):
     #    list.append([input('Ingrese 9 numeros para rellenar la fila: ')])
     
     for j in range(9):
         for k in range(9):
-            print(list[j][k])
+            print(list[j][k],end=' ')
             if int(list[j][k]) > 9:
                 return 'Los numeros ingresados no deben ser mayor a 9!'
-            if list[j].find(k+1) == -1:
+            if list[j].find(str(k+1)) == -1:
                 return 'Los numeros no se deben repetir!'
+        print()
     if ctester():
         return 'Si'
     
-    
-sudoku()
+print(sudoku())
