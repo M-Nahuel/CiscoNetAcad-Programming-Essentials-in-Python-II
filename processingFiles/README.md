d5d3f702d46c865f3e265c1d58a565c1804e8b40
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-04-29 18:59:49 -0400

    store the output in a file

---
+++ processingFiles/README.md
@@ -0,0 +1,3 @@
+# Latin Counter
+
+This program takes a name of a file as input; counts how many times each latin character appears(not case sensitive), and prints a histogram which is also stored in another file with the same name as the input, but with a `.hist` extension.
--- processingFiles/latinCounter.py
+++ processingFiles/latinCounter.py
@@ -3,10 +3,22 @@ from os import strerror
 def islatin(ch):
     if ord(ch.upper()) not in range(65, 91):
         return False
+from os import strerror
+
+def isLatin(ch):
+    if ord(ch.upper()) not in range(65, 91):
+        return False
     return True
 
+def list(ch):
+    if ch not in chars:
+        chars[ch] = 1
+    else:
+        chars[ch] += 1
 
-inp = input('Ingrese el nombre del archivo que desea inspeccionar.')
+chars = {}
+
+inp = input('Ingrese el nombre del archivo que desea inspeccionar: ')
 try:
     src = open(inp, 'rt')
 except IOError as e:
@@ -20,11 +32,25 @@ try:
     if len(lines) > 0:
         for line in lines:
             for ch in line:
-                if islatin(ch):
+                if isLatin(ch):
+                    list(ch)
                     count += 1
 except IOError as e:
-    print('No se pudo terminar el proceso.')
+    print('No se pudo terminar el proceso.', strerror(e.errno))
     exit(e.error)
 
 print(count, 'Letras latinas contadas.')
+print('Cantidad de cada letra: ')
+try:
+    wrt = open(inp + '.hist', 'wt')
+except IOError as e:
+    print('No se pudo escribir el histograma.', strerror(e.errno))
+    exit(e.error)
+
+for i in sorted(chars, key = lambda a : chars[a], reverse = True):
+    output = i + ' -> ' + str(chars[i])
+    wrt.write(output + '\n')
+    print(output)
 src.close()
+wrt.close()
+
